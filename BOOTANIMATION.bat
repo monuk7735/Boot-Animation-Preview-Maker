b9f6e4622e39fedfdff63152edba88c1c08e38b3
Monu Kumar <monuk7735@gmail.com>
2020-05-15 22:34:47 +0530

    Initial Commit

---
+++ BOOTANIMATION.bat
@@ -0,0 +1,48 @@
+:: Don't Edit This File Unless You Know What You are Doing
+@echo OFF
+
+
+CALL :HEADER_TEXT
+
+echo.Checking Files
+
+set input_file_path = "input\"
+set output_file_path = "output\"
+
+if not exist "output\" mkdir "output\"
+if not exist "input\bootanimation\" mkdir "input\bootanimation\"
+
+
+if exist "input\bootanimation\desc.txt" echo.&set /P existing=Existing Files Found. Use Existing Files? [y/n]
+if /I "%existing%"=="y" goto :SHOW_MENU
+
+:DELETE_FILES
+if exist "input\bootanimation\desc.txt" rd /s /q input\bootanimation
+if exist "input\bootanimation\desc.txt" rd /s /q input\bootanimation
+if exist "input\bootanimation\desc.txt" rd /s /q input\bootanimation
+
+:SHOW_MENU
+cls
+CALL :HEADER_TEXT
+echo.1. Create Bootanimation Preview (for input\bootanimation or input\bootanimation.zip)
+echo.2. Create bootanimation.zip (from input\bootanimation)
+echo.3. Exit
+echo.&echo.&set /P INPUT=Choose any Option():
+
+if /I "%INPUT%" == "1" bin\python\Scripts\python.exe bin/create_preview.py & pause
+if /I "%INPUT%" == "2" bin\python\Scripts\python.exe bin/create_bootanimation.py & pause
+if /I "%INPUT%" == "3" pause & EXIT
+
+goto :SHOW_MENU
+
+:HEADER_TEXT 
+echo. =============================================
+echo.        Boot Animation Preview Creator
+echo.
+echo.                  by monuk7735      
+echo.
+echo.
+echo. =============================================
+echo.
+if /I "%existing%"=="y" echo. Using Existing Files in input/bootanimation/ & echo. &echo. =============================================  & echo.
+EXIT /B 0
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Boot-Animation-Preview-Maker
-Create Video Preview of 
+>Create Video Preview of bootanimation.zip of Android OS
+
+#####How to use:
+
+Preview Bootanimation.zip
+
+```
+Clone the Repo
+Place bootanimation.zip file in input\ of the Project
+Run create_preview.py Script
+Preview.avi file will be placed in /output
+```
+
+Create Your Own Bootanimation.zip
+
+```
+Clone the Repo
+Create input/bootanimation/
+Place Files according to this [Guide](https://android.googlesource.com/platform/frameworks/base/+/master/cmds/bootanimation/FORMAT.md)
+Run Script for Preview and to create bootanimation.zip
+Preview.avi bootanimation.zip files will be placed in /output
+```
